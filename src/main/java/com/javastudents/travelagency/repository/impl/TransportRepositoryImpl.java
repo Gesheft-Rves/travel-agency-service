0b2b6a1c2266b0bc80f1befda9d4fbd23e951c56
ruslan <aslanov.r.m@gmail.com>
2018-12-19 13:32:21 +0300

    sql queries in TransportRepositoryImpl

--- src/main/java/com/javastudents/travelagency/repository/impl/TransportRepositoryImpl.java
+++ src/main/java/com/javastudents/travelagency/repository/impl/TransportRepositoryImpl.java
@@ -2,7 +2,9 @@ package com.javastudents.travelagency.repository.impl;
 
 import com.javastudents.travelagency.entity.Transport;
 import com.javastudents.travelagency.repository.CrudRepository;
+import org.intellij.lang.annotations.Language;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 
@@ -18,21 +20,49 @@ public class TransportRepositoryImpl implements CrudRepository<Transport> {
 
     @Override
     public void create(Transport transport) {
+        @Language("MySQL")
+        String query = "INSERT INTO transport (name, description, passenger_seat_qty) VALUES (?,?,?)";
 
+        jdbcTemplate.update(
+                query,
+                transport.getName(),
+                transport.getDescription(),
+                transport.getPassengerSeatQty()
+        );
     }
 
     @Override
     public Transport read(int transportId) {
-        return null;
+        @Language("MySQL")
+        String query = "SELECT * FROM transport WHERE transport_id=?";
+
+        try {
+            return jdbcTemplate.queryForObject(
+                    query,
+                    new Object[]{transportId},
+
+                    (rs, rowNum) -> Transport.builder()
+                            .name(rs.getString("name"))
+                            .build()
+            );
+        } catch (EmptyResultDataAccessException e) {
+            return null;
+        }
     }
 
     @Override
     public void update(Transport transport) {
+        @Language("MySQL")
+        String query = "UPDATE transport SET name = ?, description = ?, passenger_seat_qty = ? WHERE tour_category_id = ?";
 
+        jdbcTemplate.update(query, transport.getName(), transport.getDescription(), transport.getPassengerSeatQty(), transport.getId());
     }
 
     @Override
     public void delete(int transportId) {
+        @Language("MySQL")
+        String query = "DELETE FROM transport WHERE transport_id = ?";
 
+        jdbcTemplate.update(query, transportId);
     }
 }
