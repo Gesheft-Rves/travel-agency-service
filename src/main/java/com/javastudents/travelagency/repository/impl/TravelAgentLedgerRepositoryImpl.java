93fafe5389a9469f20e9fcdeb7ded9d9dc9d5883
ruslan <aslanov.r.m@gmail.com>
2018-12-19 14:52:09 +0300

    sql queries in TravelAgentLedgerRepositoryImpl

--- src/main/java/com/javastudents/travelagency/repository/impl/TravelAgentLedgerRepositoryImpl.java
+++ src/main/java/com/javastudents/travelagency/repository/impl/TravelAgentLedgerRepositoryImpl.java
@@ -4,6 +4,7 @@ import com.javastudents.travelagency.entity.TravelAgentLedger;
 import com.javastudents.travelagency.repository.TravelAgentLedgerRepository;
 import org.intellij.lang.annotations.Language;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 
@@ -22,23 +23,46 @@ public class TravelAgentLedgerRepositoryImpl implements TravelAgentLedgerReposit
     @Override
     public void create(TravelAgentLedger travelAgentLedger) {
         @Language("MySQL")
-        String query = "INSERT INTO travel_agent_ledger (travel_agent_id, operatin_date, purchase_id, amount) VALUES (?)";
+        String query = "INSERT INTO travel_agent_ledger (travel_agent_id, operation_date, purchase_id, amount) VALUES (?, ?, ?)";
 
-        jdbcTemplate.update(query);
+        jdbcTemplate.update(query, travelAgentLedger.getTravelAgentId(), travelAgentLedger.getOperationDate(), travelAgentLedger.getPurchaseId(), travelAgentLedger.getAmount());
     }
 
     @Override
     public TravelAgentLedger read(int travelAgentLedgerId) {
-        return null;
+        @Language("MySQL")
+        String query = "SELECT * FROM transport_seat WHERE transport_seat_id=?";
+
+        try {
+            return jdbcTemplate.queryForObject(
+                    query,
+                    new Object[]{travelAgentLedgerId},
+
+                    (rs, rowNum) -> TravelAgentLedger.builder()
+                            .travelAgentId(rs.getInt("travel_agent_id"))
+                            .operationDate(rs.getTimestamp("operation_date"))
+                            .purchaseId(rs.getInt("purchase_id"))
+                            .amount(rs.getBigDecimal("amount"))
+                            .build()
+            );
+        } catch (EmptyResultDataAccessException e) {
+            return null;
+        }
     }
 
     @Override
     public void update(TravelAgentLedger travelAgentLedger) {
+        @Language("MySQL")
+        String query = "UPDATE travel_agent_ledger SET travel_agent_id = ?, operation_date = ?, purchase_id = ?, amount = ? WHERE travel_agent_ledger_id = ?";
 
+        jdbcTemplate.update(query, travelAgentLedger.getTravelAgentId(), travelAgentLedger.getOperationDate(), travelAgentLedger.getPurchaseId(), travelAgentLedger.getAmount(), travelAgentLedger.getId());
     }
 
     @Override
     public void delete(int travelAgentLedgerId) {
+        @Language("MySQL")
+        String query = "DELETE FROM travel_agent_ledger WHERE travel_agent_ledger_id = ?";
 
+        jdbcTemplate.update(query, travelAgentLedgerId);
     }
 }
