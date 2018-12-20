9410776c7abecbaafc4b60f546af9d07d120e985
Gesheft-Rves <Rves07@yandex.ru>
2018-12-20 10:41:53 +0300

    add repositories

---
+++ src/main/java/com/javastudents/travelagency/repository/AppUserRoleRepository.java
@@ -0,0 +1,6 @@
+package com.javastudents.travelagency.repository;
+
+import com.javastudents.travelagency.entity.AppUserRole;
+
+public interface AppUserRoleRepository extends CrudRepository<AppUserRole> {
+}
