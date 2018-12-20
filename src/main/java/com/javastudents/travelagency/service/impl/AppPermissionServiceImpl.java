fb38f2b8ba511f4b4b47a8f7d514daeadcb48288
Gesheft-Rves <Rves07@yandex.ru>
2018-12-20 17:35:22 +0300

    add CRUD methods and tests for AppPermission

---
+++ src/main/java/com/javastudents/travelagency/service/impl/AppPermissionServiceImpl.java
@@ -0,0 +1,37 @@
+package com.javastudents.travelagency.service.impl;
+
+import com.javastudents.travelagency.entity.AppPermission;
+import com.javastudents.travelagency.repository.AppPermissionRepository;
+import com.javastudents.travelagency.service.AppPermissionService;
+import org.springframework.beans.factory.annotation.Autowired;
+
+public class AppPermissionServiceImpl implements AppPermissionService {
+
+    private final AppPermissionRepository repository;
+
+    @Autowired
+    public AppPermissionServiceImpl(AppPermissionRepository repository) {
+        this.repository = repository;
+    }
+
+
+    @Override
+    public void create(AppPermission appPermission) {
+        repository.create(appPermission);
+    }
+
+    @Override
+    public AppPermission read(int appPermissionId) {
+        return repository.read(appPermissionId);
+    }
+
+    @Override
+    public void update(AppPermission appPermission) {
+        repository.update(appPermission);
+    }
+
+    @Override
+    public void delete(int appPermissionId) {
+        repository.delete(appPermissionId);
+    }
+}
