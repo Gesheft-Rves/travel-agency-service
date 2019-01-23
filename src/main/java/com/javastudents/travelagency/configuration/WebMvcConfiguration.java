c5d5044d8e45fe8a76bbdd7f3df5f52dd44146b2
ruslan <aslanov.r.m@gmail.com>
2019-01-23 19:53:44 +0300

    update after merge pull request

---
+++ src/main/java/com/javastudents/travelagency/configuration/WebMvcConfiguration.java
@@ -0,0 +1,81 @@
+package com.javastudents.travelagency.configuration;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.hibernate4.Hibernate4Module;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ReloadableResourceBundleMessageSource;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+import org.springframework.web.servlet.config.annotation.*;
+import org.springframework.web.servlet.i18n.CookieLocaleResolver;
+import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
+
+import java.util.List;
+import java.util.Locale;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan
+public class WebMvcConfiguration implements WebMvcConfigurer {
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(localeChangeInterceptor());
+    }
+
+    @Bean
+    public LocaleChangeInterceptor localeChangeInterceptor() {
+        LocaleChangeInterceptor localeChangeInterceptor = new LocaleChangeInterceptor();
+        localeChangeInterceptor.setParamName("lang");
+        return localeChangeInterceptor;
+    }
+
+    @Bean(name = "localeResolver")
+    public CookieLocaleResolver localeResolver() {
+        CookieLocaleResolver localeResolver = new CookieLocaleResolver();
+        Locale defaultLocale = new Locale("en");
+        localeResolver.setDefaultLocale(defaultLocale);
+        return localeResolver;
+    }
+
+    @Bean
+    public ReloadableResourceBundleMessageSource messageSource() {
+        ReloadableResourceBundleMessageSource messageSource = new ReloadableResourceBundleMessageSource();
+        messageSource.setBasename("classpath:lang/messages");
+        messageSource.setDefaultEncoding("UTF-8");
+        messageSource.setCacheSeconds(10); //reload messages every 10 seconds
+        return messageSource;
+    }
+
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/**").addResourceLocations("classpath:/static/");
+        WebMvcConfigurer.super.addResourceHandlers(registry); //под вопросом
+    }
+
+    //More configuration....
+
+    /* Here we register the Hibernate4Module into an ObjectMapper, then set this custom-configured ObjectMapper
+     * to the MessageConverter and return it to be added to the HttpMessageConverters of our application*/
+    public MappingJackson2HttpMessageConverter jacksonMessageConverter(){
+        MappingJackson2HttpMessageConverter messageConverter = new MappingJackson2HttpMessageConverter();
+
+        ObjectMapper mapper = new ObjectMapper();
+        mapper.registerModule(new Hibernate4Module());
+
+        messageConverter.setObjectMapper(mapper);
+        return messageConverter;
+
+    }
+
+    @Override
+    public void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
+        //Here we add our custom-configured HttpMessageConverter
+        converters.add(jacksonMessageConverter());
+        WebMvcConfigurer.super.configureMessageConverters(converters); //под вопросом
+    }
+
+}
---
+++ src/main/java/com/javastudents/travelagency/controller/MainController.java
@@ -0,0 +1,12 @@
+package com.javastudents.travelagency.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class MainController {
+    @RequestMapping({"/", "/home"})
+    public String index() {
+        return "index";
+    }
+}
