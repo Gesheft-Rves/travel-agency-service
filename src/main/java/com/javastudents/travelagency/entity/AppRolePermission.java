e40e401f3a29b98d510198d0bdf78c908a04d549
Gesheft-Rves <Rves07@yandex.ru>
2019-03-05 20:19:55 +0300

    AppRolePermission Controller and WebPages

--- src/main/java/com/javastudents/travelagency/entity/AppRolePermission.java
+++ src/main/java/com/javastudents/travelagency/entity/AppRolePermission.java
@@ -11,4 +11,13 @@ public class AppRolePermission implements Entity {
     private Integer appRolePermissionId;
     private Integer appRoleId;
     private Integer appPermissionId;
+
+    public AppRolePermission(Integer appRolePermissionId, Integer appRoleId, Integer appPermissionId) {
+        this.appRolePermissionId = appRolePermissionId;
+        this.appRoleId = appRoleId;
+        this.appPermissionId = appPermissionId;
+    }
+
+    public AppRolePermission() {
+    }
 }
