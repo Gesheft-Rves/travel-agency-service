0d6e36db400bc2177a84524867267b10b70257c6
ruslan <aslanov.r.m@gmail.com>
2019-03-20 15:05:16 +0300

    TravelAgency controller created, added list() methods for service and repository, added templates for webUI

--- src/main/java/com/javastudents/travelagency/service/TravelAgencyService.java
+++ src/main/java/com/javastudents/travelagency/service/TravelAgencyService.java
@@ -2,5 +2,8 @@ package com.javastudents.travelagency.service;
 
 import com.javastudents.travelagency.entity.TravelAgency;
 
+import java.util.List;
+
 public interface TravelAgencyService extends CrudService<TravelAgency> {
+    List<TravelAgency> list();
 }
